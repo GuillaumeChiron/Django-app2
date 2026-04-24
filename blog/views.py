82f7a52596b174d366420cac892e15b7a2f8e545
guillaume <guillaumechiron4@gmail.com>
2026-04-24 11:38:51 +0200

    ajout de la pagination

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Q
+from django.core.paginator import Paginator
 from django.forms import formset_factory
 
 from itertools import chain
@@ -21,9 +22,12 @@ def home_page(request):
     blogs_and_photos = sorted(
         chain(blogs, photos), key=lambda x: x.date_created, reverse=True
     )
-    return render(
-        request, "blog/home_page.html", {"blogs_and_photos": blogs_and_photos}
-    )
+
+    paginator = Paginator(blogs_and_photos, 2)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "blog/home_page.html", {"page_obj": page_obj})
 
 
 @login_required
